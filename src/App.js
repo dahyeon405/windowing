807a3c4ed7ce683fb46ea27e807e663c53398059
dahyeon405 <dahyeon405@naver.com>
2023-01-31 13:02:25 +0900

    chore: 샘플 코드 추가

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import FixedSizeList from "./FixedSizeList.tsx";
+import Item from "./Item.tsx";
 
 function App() {
+  const data = [];
+  for (let i = 0; i < 100; i++) {
+    data.push(i);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FixedSizeList scrollTarget={window} top={0} itemHeight={200} itemData={data} overscanCount={3}>
+        <Item />
+      </FixedSizeList>
     </div>
   );
 }
---
+++ src/Item.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Item({ index }) {
+  return <div>{index}</div>;
+}
