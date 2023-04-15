8e3492c6f0d3885a602b0f0fa90d5ad9d10f6db5
dahyeon405 <dahyeon405@naver.com>
2023-04-15 17:35:08 +0900

    style/ 샘플 파일 스타일링 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import FixedSizeList from "./lib/components/FixedSizeList.tsx";
+import FixedSizeList from "./lib/components/FixedSizeList/FixedSizeList.tsx";
 import Item from "./Item.tsx";
 
 function App() {
@@ -7,10 +7,12 @@ function App() {
     data.push(i);
   }
 
+  const sampleProp = "can pass props!";
+
   return (
     <div className="App">
-      <FixedSizeList scrollTarget={window} top={0} itemHeight={200} itemData={data} overscanCount={2}>
-        <Item />
+      <FixedSizeList scrollTarget={window} top={0} itemHeight={100} itemData={data} overscanCount={10}>
+        <Item sampleProp={sampleProp} />
       </FixedSizeList>
     </div>
   );
